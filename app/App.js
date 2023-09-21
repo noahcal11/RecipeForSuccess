83b642c609ad683482b81ccdb8d8b9c284ede2ea
Noah C <noahcal48@gmail.com>
2023-09-21 14:40:41 -0400

    fix to work with jeff's stuff

--- app/App.js
+++ app/App.js
@@ -1,6 +1,10 @@
 import { useEffect,useState } from 'react';
 import Login from "./views/login";
-import Home from './views/home';
+import Home from "./views/home";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [recipes, setRecipes] = useState([]);
@@ -19,8 +23,14 @@ export default function App() {
   }
 
   return (
-    // <Login recipes={recipes} />
-    <Home />
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 
 };
--- app/views/home.js
+++ app/views/home.js
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar'; 
 import { Text, Image, View, TouchableOpacity, TextInput } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import NavigationBar from '../Components/NavBar';
-import SearchBar from '../Components/SearchBar';
 import Banner from '../Components/Banner';
 
 EStyleSheet.build();
 
-export default function Home(){
+export default function Home({ navigation, route }){
     return(
         <View>
             <Banner title="Home" />
+            <Text>Welcome {route.params.username}!</Text>
+            <TouchableOpacity
+                onPress={() => {
+                    navigation.navigate("Login")
+                }}
+                >
+                <Text>Go Back</Text>
+            </TouchableOpacity>
         </View>
     );
 }
