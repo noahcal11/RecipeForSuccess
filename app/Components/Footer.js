05abddcbeb2d52a6f98cb262eede4454cc05623a
rt1252 <112960691+rt1252@users.noreply.github.com>
2023-10-04 20:03:03 -0400

    Updated footer code

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -1,11 +1,26 @@
+import { Text, View, TouchableOpacity, FlatList, SafeAreaView, StyleSheet, Modal } from "react-native";
 import React from 'react';
-import { View, Text } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import HomeIcon from "../assets/svg/home";
+import HeartIcon from "../assets/svg/heart";
+import RibbonIcon from "../assets/svg/ribbon"
 
 const Footer = () => {
     return (
       <View style={styles.footerContainer}>
-        <Text style={styles.footerText}>This is the footer.</Text>
+
+        {/* This is working when I move to styles it is not working */}
+        <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')} style={{marginHorizontal: "12.5%"}}>
+            <HeartIcon width="40" height='100' stroke="black" strokeWidth="0.25"/>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')} style={{marginHorizontal: "12.5%"}}>
+            <HomeIcon style={styles.icons}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')} style={{marginHorizontal: "12.5%"}}>
+            <RibbonIcon style={styles.icons}/>
+        </TouchableOpacity>
       </View>
     );
   };
@@ -16,11 +31,14 @@ const Footer = () => {
   const styles = EStyleSheet.create({
     footerContainer: {
       backgroundColor: '#F74F4F',
-      padding: 10,
       alignItems: 'center',
+      flexDirection: 'row',
+      justifyContent: 'center',
     },
-    footerText: {
-      color: 'white',
-      fontSize: 16,
-    },
+    icons: {
+      width: '40',
+      height: '100',
+      stroke: 'black',
+      strokeWidth: '0.25'
+    }
   })
---
+++ app/assets/svg/home.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+const HomeIcon = (props) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="ionicon"
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <Path
+      d="M80 212v236a16 16 0 0016 16h96V328a24 24 0 0124-24h80a24 24 0 0124 24v136h96a16 16 0 0016-16V212"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={32}
+    />
+    <Path
+      d="M480 256L266.89 52c-5-5.28-16.69-5.34-21.78 0L32 256M400 179V64h-48v69"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={32}
+    />
+  </Svg>
+);
+export default HomeIcon;
