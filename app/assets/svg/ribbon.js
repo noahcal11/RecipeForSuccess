71eb95468c04079f417029ae2a3ba33e464ab06b
rt1252 <112960691+rt1252@users.noreply.github.com>
2023-10-05 14:33:16 -0400

    Updated icons working

--- app/assets/svg/ribbon.js
+++ app/assets/svg/ribbon.js
@@ -12,24 +12,24 @@ const RibbonIcon = (props) => (
       cx={256}
       cy={160}
       r={128}
-      //fill="none"
-      stroke="currentColor"
+      fill="none"
+      stroke="black"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={32}
     />
     <Path
       d="M143.65 227.82L48 400l86.86-.42a16 16 0 0113.82 7.8L192 480l88.33-194.32"
-      //fill="none"
-      stroke="currentColor"
+      fill="none"
+      stroke="black"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={32}
     />
     <Path
       d="M366.54 224L464 400l-86.86-.42a16 16 0 00-13.82 7.8L320 480l-64-140.8"
-      //fill="none"
-      stroke="currentColor"
+      fill="none"
+      stroke="black"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={32}
@@ -39,7 +39,7 @@ const RibbonIcon = (props) => (
       cy={160}
       r={64}
       //fill="none"
-      stroke="currentColor"
+      stroke="black"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={32}
