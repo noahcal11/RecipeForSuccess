0dd1714e76f0f1b5ec7e6e6ec5382dffcbcd8ba0
rt1252 <112960691+rt1252@users.noreply.github.com>
2023-10-26 15:11:58 -0400

    Updated recipe progression and survey to include footer

--- app/Components/RecipeProgression.js
+++ app/Components/RecipeProgression.js
@@ -5,6 +5,8 @@ import CheckBox from 'expo-checkbox';
 import { useState, useContext } from 'react';
 import { Context } from '../App'
 import Banner from './Banner';
+import Footer from '../Components/Footer'
+import { ScrollView } from 'react-native-gesture-handler';
 
 // TODO: style this page so it actually looks good
 const RecipeProgression = ({ingredients, directions, title}) => {
@@ -90,6 +92,7 @@ const RecipeProgression = ({ingredients, directions, title}) => {
         <View style={styles.container}>
             <Banner title={title} username={username} email={email}/>
             {steps()}
+            <Footer username={username} email={email} />
         </View>
     )
 }
