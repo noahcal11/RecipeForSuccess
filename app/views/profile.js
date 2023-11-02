8cfccbbcc4bba9df6e7d88f3d7f25b842d452de9
JeffSinsel <jeffsinsel@gmail.com>
2023-11-01 22:01:54 -0400

    fix the broken stuff

--- app/views/profile.js
+++ app/views/profile.js
@@ -1,11 +1,28 @@
 import BannerTitle from '../Components/Banner';
 import Footer from '../Components/Footer';
 import React from 'react';
-import { Text, View, TouchableOpacity, FlatList, SafeAreaView, StyleSheet, Modal } from "react-native";
+import { Text, View, Pressable, FlatList, SafeAreaView, StyleSheet, Modal, ScrollView } from "react-native";
 import ProfileBannerTitle from '../Components/ProfileBanner';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import { useState,useContext } from 'react';
+import { Context } from '../App'
+import DropDownPicker from 'react-native-dropdown-picker';
 
+
+
+EStyleSheet.build();
 
 export default function Profile() {
+    const {username,setUsername,email,setEmail} = useContext(Context)
+    const API_BASE = "https://recipe-api-maamobyhea-uc.a.run.app/"+process.env.REACT_APP_API_TOKEN
+
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+        {label: 'Apple', value: 'apple'},
+        {label: 'Banana', value: 'banana'}
+    ]);
+    
     return(
         <View style={styles.container}>
             <ProfileBannerTitle> </ProfileBannerTitle>
@@ -27,3 +44,13 @@ export default function Profile() {
         </View>
     )
 }
+
+const styles = EStyleSheet.create({
+    container: {
+        flex: 1,
+        height: '100%',
+      },
+    dropDown: {
+        alignItems: 'center',
+    }
+})
