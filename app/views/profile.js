1d3010056b11b437fd4e940ef95e39736bd77771
rt1252 <112960691+rt1252@users.noreply.github.com>
2023-11-02 14:39:54 -0400

    work on profile js

--- app/views/profile.js
+++ app/views/profile.js
@@ -1,4 +1,3 @@
-import BannerTitle from '../Components/Banner';
 import Footer from '../Components/Footer';
 import React from 'react';
 import { Text, View, Pressable, FlatList, SafeAreaView, StyleSheet, Modal, ScrollView } from "react-native";
@@ -7,7 +6,7 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import { useState,useContext } from 'react';
 import { Context } from '../App'
 import DropDownPicker from 'react-native-dropdown-picker';
-
+import global from '../Genstyle';
 
 
 EStyleSheet.build();
@@ -17,22 +16,29 @@ export default function Profile() {
     const API_BASE = "https://recipe-api-maamobyhea-uc.a.run.app/"+process.env.REACT_APP_API_TOKEN
 
     const [open, setOpen] = useState(false);
-    const [value, setValue] = useState(null);
-    const [items, setItems] = useState([
-        {label: 'Apple', value: 'apple'},
-        {label: 'Banana', value: 'banana'}
+    const [value, setValue] = useState([]);
+    const [DietItems, setItems] = useState([
+        {label: 'Lactose intolerance', value: 'Lactose intolerance'},
+        {label: 'Gluten Free', value: 'Gluten Free'},
+        {label: 'Vegeterian', value: 'Vegeterian'},
+        {label: 'Vegan', value: 'Vegan'},
+        {label: 'Kosher', value: 'Kosher'},
+        {label: 'Keto', value: 'Keto'},
+        {label: 'Dairy Free', value: 'Dairy Free'},
     ]);
     
     return(
-        <View style={styles.pageContainer}>
+        <View style={global.container}>
             <ProfileBannerTitle> </ProfileBannerTitle>
                     <View style={styles.settingsContainer}>
                         <View>
+                        <Text style={{...global.subheaderText}}>Dietary Preferences</Text>
                             <DropDownPicker
                                 autoScroll={true}
+                                multiple={true}
                                 open={open}
                                 value={value}
-                                items={items}
+                                items={DietItems}
                                 setOpen={setOpen}
                                 setValue={setValue}
                                 setItems={setItems}
@@ -45,10 +51,6 @@ export default function Profile() {
 }
 
 const styles = EStyleSheet.create({
-    pageContainer: {
-        flex: 1,
-        height: '100%',
-      },
     settingsContainer: {
         height: '80%',
         alignItems: 'center',
