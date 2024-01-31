3a33bb3128ff3ee80ef765d7a2bf14907bd854ac
JeffSinsel <jeffsinsel@gmail.com>
2024-01-30 20:30:18 -0500

    Revert "updates"
    This reverts commit 1e0c980d16f25921f0647959359c6c0d24f5ef84.

--- app/App.js
+++ app/App.js
@@ -1,12 +1,12 @@
 import { useEffect, useState, createContext } from 'react';
-import Login from "./views/Login";
+import Login from "./views/login";
 import Home from "./views/home";
 import Skills from "./views/skills";
 import recipePages from "./views/RecipePages";
-import Profile from "./views/Profile";
+import Profile from "./views/profile";
 import Favorites from './views/favorites';
 import searchResults from './views/SearchResults'
-import PageTemplate from './views/PageTemplate';
+import PageTemplate from './views/pageTemplate';
 import Created from './views/Created';
 import Completed from './views/Completed';
 import Upload from './views/Upload';
