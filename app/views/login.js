a2236d06a3f52ba9692b1bc4c18de37a440e034b
rt1252 <112960691+rt1252@users.noreply.github.com>
2024-02-20 19:57:08 -0500

    text limits on all fields on login

--- app/views/login.js
+++ app/views/login.js
@@ -6,6 +6,7 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import { Context } from '../Context';
 import global from '../Genstyle';
 import LogoIcon from '../assets/svg/logo';
+import {AutoGrowingTextInput} from 'react-native-autogrow-textinput';
 
 EStyleSheet.build();
 
@@ -21,6 +22,10 @@ export default function Login({navigation}) {
   const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
   const [emailErrorMessage, setEmailErrorMessage] = useState('');
   const {username,setUsername,email,setEmail,setFavorited,setCompleted,setCreated,setVisibleWidgets} = useContext(Context)
+  const [maxEmailError, setMaxEmailError] = useState('');
+  const [maxPasswordError, setMaxPasswordError] = useState('');
+  const [createMaxUserError, setCreateMaxUserError] = useState('');
+
 
   const API_BASE = "https://recipe-api-maamobyhea-uc.a.run.app/"+process.env.REACT_APP_API_TOKEN
 
@@ -92,18 +97,22 @@ export default function Login({navigation}) {
     }).then(getUser()).catch(err => console.error(err));
   }
 
-  const validateIsEmail = async (email) => {
-    const isValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-    setEmailValid(isValid);
-    setEmailErrorMessage(isValid ? '' : 'Invalid email format');
-    return isValid;
+
+  const validateIsEmail = (email) => {
+    const isLengthValid = email.length <= 50;
+    const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    setEmailValid(isValid && isLengthValid);
+    setEmailErrorMessage(isValid && isLengthValid ? '' : isLengthValid ? 'Invalid email format' : 'Maximum character limit reached');
+    return isValid && isLengthValid;
   };
 
   const validatePassword = (password) => {
+    const isLengthValid = password.length <= 50;
     const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
     const isValid = passwordRegex.test(password);
-    setIsPasswordValid(isValid);
-    setPasswordErrorMessage(isValid ? '' : 'Password must have at least 8 characters with at least one uppercase letter, one number, and one symbol.');
+    setIsPasswordValid(isValid && isLengthValid);
+    //setPasswordErrorMessage(isValid ? '' : 'Password must have at least 8 characters with at least one uppercase letter, one number, and one symbol.');
+    setPasswordErrorMessage(isValid && isLengthValid ? '' : isLengthValid ? 'Password must have at least 8 characters with at least one uppercase letter, one number, and one symbol.' : 'Maximum character limit reached');
     return isValid;
   };  
 
@@ -113,18 +122,42 @@ export default function Login({navigation}) {
           return(
           <View>
             <TextInput
-              style={global.input}
-              onChangeText={setEmail}
+              style={[global.input, maxEmailError && { marginBottom: 0 }, {padding:0}]}
+              //onChangeText={setEmail}
+              maxLength={50}
               value={email}
               placeholder="Email"
+
+              onChangeText={(text) => {
+                                
+                if (text.length === 50) {
+                    setMaxEmailError('Maximum character limit reached');
+                } else if (maxEmailError && text.length < 50) {
+                  setMaxEmailError('');
+                }       
+                setEmail(text);
+            }}
             />
+            {maxEmailError ? <Text style={{ ...styles.bodyText, color: 'red' }}>{maxEmailError}</Text> : null}
+
             <TextInput
-              style={global.input}
-              onChangeText={setPassword}
+              style={[global.input, maxPasswordError && { marginBottom: 0 }, {padding:0}]}
+              //onChangeText={setPassword}
+              maxLength={50}
               value={password}
               placeholder="Password"
               secureTextEntry={true}
+              onChangeText={(text) => {
+                                
+                if (text.length === 50) {
+                    setMaxPasswordError('Maximum character limit reached');
+                } else if (maxEmailError && text.length < 50) {
+                  setMaxPasswordError('');
+                }       
+                setPassword(text);
+            }}
             />
+            {maxPasswordError ? <Text style={{ ...styles.bodyText, color: 'red' }}>{maxPasswordError}</Text> : null}
             <Pressable
               style={global.button}
               onPress={() => {
@@ -140,6 +173,7 @@ export default function Login({navigation}) {
               <View>
                 <TextInput
                   style={[global.input, !emailValid && { marginBottom: 0 }, {padding:0}]}
+                  maxLength={51}
                   onChangeText={(text) => {
                     setEmail(text);
                     validateIsEmail(text);
@@ -151,13 +185,25 @@ export default function Login({navigation}) {
                   <Text style={[global.centerBodyText, {flex: 0, margin: '0%', color: 'red'}]}>{emailErrorMessage}</Text>
                 )}
                 <TextInput
-                  style={global.input}
-                  onChangeText={setUsername}
+                  style={[global.input, createMaxUserError && { marginBottom: 0 }, {padding:0}]}
+                  maxLength={50}
                   value={username}
                   placeholder="Username"
+                  
+                  onChangeText={(text) => {
+                                
+                    if (text.length === 50) {
+                        setCreateMaxUserError('Maximum character limit reached');
+                    } else if (createMaxUserError && text.length < 50) {
+                      setCreateMaxUserError('');
+                    }       
+                    setUsername(text)
+                  }}
                 />
+                {createMaxUserError ? <Text style={{ ...styles.bodyText, color: 'red' }}>{createMaxUserError}</Text> : null}
                 <TextInput
                   style={[global.input, !emailValid && { marginBottom: 0 }]}
+                  maxLength={51}
                   onChangeText={(text) => {
                     setPassword(text);
                     validatePassword(text);
@@ -379,5 +425,14 @@ const styles = EStyleSheet.create({
       color: 'black',
       fontSize: '0.9rem',
       fontFamily: 'Cairo_500Medium',
-    }
+    },
+    bodyText: {
+      minWidth: 20,
+      color: 'black',
+      fontSize: '1rem',
+      fontFamily: 'Cairo_500Medium',
+      flex: 0.3,
+      paddingLeft: '0.5rem',
+      textAlign: 'center',
+    },
   });
