402019bcce5060d58fcf3e828e3116afa800cd99
Noah C <noahcal48@gmail.com>
2024-03-05 20:52:43 -0500

    Update Home.js

--- app/views/Home.js
+++ app/views/Home.js
@@ -86,7 +86,7 @@ export default function Home({ navigation, route }) {
     }, [email, setProfileAllergies]);
 
     useEffect(() => {
-        //console.log(profileAllergies);
+        
     }, [profileAllergies]);
 
 
